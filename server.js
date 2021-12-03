3448e9d4e3fe0762987120b9118c180414105195
FranklinC01 <fcordero@unab.edu.co>
2021-12-03 11:58:52 -0500

    new

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/src/app/components/listar-usuario/listar-usuario.component.html'));
 });
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);

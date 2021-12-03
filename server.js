1ce135ed8cb36a0b9acf0f42c88413fe9488e674
FranklinC01 <fcordero@unab.edu.co>
2021-12-03 11:46:23 -0500

    server change

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/src'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/src/index.html'));
+res.sendFile(path.join(__dirname+'/src/app/components/listar-usuario/listar-usuario.component.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
